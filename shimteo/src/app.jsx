f09a04b8680b9bf305d271245af17840566d6914
songhyunkwang <songhyeongwang@songhyeongwangs-MacBook-Pro.local>
2025-04-09 20:32:05 +0900

    start현광

--- shimteo/src/app.jsx
+++ shimteo/src/app.jsx
@@ -1,9 +1,9 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './Signin';
-import SignUp from './Signup';
-// import OtherFeature from './components/OtherFeature';
+import SignIn from './user/Signin';
+import SignUp from './user/Signup';
+
 
 function App() {
   const isLoggedIn = false; // 나중엔 이걸 로그인 상태로 바꿔줄 수 있어!
@@ -15,7 +15,6 @@ function App() {
         <Route path="/" element={isLoggedIn ? <OtherFeature /> : <Navigate to="/signin" />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/feature" element={<OtherFeature />} />
         {/* 그 외 경로는 Not Found 처리도 가능 */}
         <Route path="*" element={<div>404 - 페이지를 찾을 수 없습니다.</div>} />
       </Routes>
